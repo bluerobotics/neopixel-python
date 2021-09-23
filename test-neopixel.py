22b63df16c6eefdec3e9d3bafc58230e33d1775c
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 16:54:28 +1000

    test-neopixel: use default parser

--- test-neopixel.py
+++ test-neopixel.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 
-import argparse
 from neopixel import NeoPixel, RED, GREEN, BLUE
-import llog
-from pathlib import Path
-from time import sleep, time
+from llog imoprt LLogWriter
+import time
 
 
 device = "neopixel"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=1)
+parser = LLogWriter.create_default_parser(__file__, device, default_frequency=1)
 parser.add_argument('--pixels', action='store', type=int, default=1)
 args = parser.parse_args()
 
 
 with llog.LLogWriter(args.meta, args.output) as log:
     neopixel = NeoPixel()
+    
+    if args.frequency:
+        delay = 1 / args.frequency
 
-    while True:
+    start_time = time.time()
+    while time.time() - start_time < args.duration:
         for color in [RED, GREEN, BLUE]:
-            # output the same color to all leds
-            neopixel.write([color]*args.pixels)
-            # extract RGB components (neopixel uses G/R/B control order)
-            g, r, b = color
-            log.log(llog.LLOG_DATA, f"{args.pixels} {r} {g} {b}")
-
-            # give the viewer some time to appreciate it
-            sleep(1.0/args.frequency)
+            try:
+                # output the same color to all leds
+                neopixel.write([color]*args.pixels)
+                # extract RGB components (neopixel uses G/R/B control order)
+                g, r, b = color
+                log.log_data(f"{args.pixels} {r} {g} {b}")
+            except Exception as e:
+                log.log_error(e)
+                if args.stop_on_error:
+                    return
+                
+            if args.frequency:
+                # give the viewer some time to appreciate it
+                time.sleep(delay)
