5524c6fd82a81ad15833c3fe7688edd1afc3eb26
Jacob Walser <jwalser90@gmail.com>
2021-09-29 02:24:17 -0400

    setup: first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='neopixel',
+    version='0.0.1',
+    description='neopixel driver',
+    author='Blue Robotics',
+    url='https://github.com/bluerobotics/neopixel-python',
+    packages=['neopixel'],
+    entry_points={
+        'console_scripts': [
+            'neopixel-test=neopixel.test:main',
+        ],
+    },
+    install_requires=['spidev'],
+)
