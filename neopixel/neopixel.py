fa02eade05ee768cb092c176fe8317499e2f8589
Jacob Walser <jwalser90@gmail.com>
2021-12-22 21:01:47 -0500

    neopixel: use a higher clock frequency to work with videocore frequencies from 500~200 MHz

--- neopixel/neopixel.py
+++ neopixel/neopixel.py
@@ -1,7 +1,7 @@
 import spidev
 
-LED_T0 = 0b11000000
-LED_T1 = 0b11110000
+LED_T0 = 0b11100000
+LED_T1 = 0b11111100
 
 GREEN = (0xFF, 0x00, 0x00)
 RED = (0x00, 0xFF, 0x00)
@@ -12,8 +12,8 @@ class NeoPixel(object):
         self._bus = spidev.SpiDev()
         # the neopixel must be the only device on the bus
         self._bus.open(bus, 0)
-        # the videocore frequency must be fixed at 500MHz
-        self._bus.max_speed_hz = 6000000
+        # this will work with videocore frequency 200~500 MHz
+        self._bus.max_speed_hz = 13000000
 
     # turn off LEDs on deletion
     def __del__(self):
