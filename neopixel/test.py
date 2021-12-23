bafd35ed1c430d426bb815ea37c13734d75e2c8e
Jacob Walser <jwalser90@gmail.com>
2021-12-22 21:02:14 -0500

    test: use a breath pattern instead of flash

--- neopixel/test.py
+++ neopixel/test.py
@@ -7,7 +7,7 @@ def main():
 
 
     device = "neopixel"
-    parser = LLogWriter.create_default_parser(__file__, device, default_frequency=1)
+    parser = LLogWriter.create_default_parser(__file__, device, default_frequency=256)
     parser.add_argument('--pixels', action='store', type=int, default=1)
     args = parser.parse_args()
 
@@ -20,21 +20,24 @@ def main():
 
         start_time = time.time()
         while time.time() < start_time + args.duration:
-            for color in [RED, GREEN, BLUE]:
-                try:
-                    # output the same color to all leds
-                    neopixel.write([color]*args.pixels)
-                    # extract RGB components (neopixel uses G/R/B control order)
-                    g, r, b = color
-                    log.log_data(f"{args.pixels} {r} {g} {b}")
-                except Exception as e:
-                    log.log_error(e)
-                    if args.stop_on_error:
-                        return
+            for color in [(1,0,0), (0,1,0), (0,0,1)]:
+                for brightness in range(256):
 
-                if args.frequency:
-                    # give the viewer some time to appreciate it
-                    time.sleep(delay)
+                    try:
+                        c = [i*brightness for i in color]
+                        # output the same color to all leds
+                        neopixel.write([c]*args.pixels)
+                        # extract RGB components (neopixel uses G/R/B control order)
+                        g, r, b = c
+                        log.log_data(f"{args.pixels} {r} {g} {b}")
+                    except Exception as e:
+                        log.log_error(e)
+                        if args.stop_on_error:
+                            return
+
+                    if args.frequency:
+                        # give the viewer some time to appreciate it
+                        time.sleep(delay)
 
 if __name__ == '__main__':
     main()
